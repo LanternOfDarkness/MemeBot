3aa4a2fe14c2e663e013c374fd304f9a1e0e2fb1
Oleksiy Velychko <anubis-96@bk.ru>
2022-10-25 19:20:08 +0300

    Added resize for image in grid

--- memer.py
+++ memer.py
@@ -33,6 +33,7 @@ class Meme:
             elif template_type == "vertical":
                 return self.__combine_image(int(w_sum/image_amount), h_sum, delta_horizontal=0)
 
+
     def __combine_image(self, width, height, delta_horizontal):
         delta_x, delta_y = 0, 0
         self.result_image = Image.new("RGBA", (width, height))
@@ -45,24 +46,48 @@ class Meme:
         return helpers.save_image(self.result_image)
 
     def __resize_images(self, composition_type):
-        min_width = min([helpers.get_size(image)[0] for image in self.images])
-        min_height = min([helpers.get_size(image)[1] for image in self.images])
 
-        for image in self.images:
-            new_image = Image.open(image)
-            x, y = new_image.size
+        if composition_type != "grid":
+            widths = [helpers.get_size(image)[0] for image in self.images]
+            heights = [helpers.get_size(image)[1] for image in self.images]
+            min_width = min(widths)
+            min_height = min(heights)
+            for image in self.images:
+                new_image = Image.open(image)
+                x, y = new_image.size
 
-            if composition_type == "single":
-                pass
-            elif composition_type == "horizontal":
-                x = (x / (y / min_height)).__int__()
-                y = min_height
-            elif composition_type == "vertical":
-                y = (y * min_width / x).__int__()
-                x = min_width
+                if composition_type == "single":
+                    pass
+                elif composition_type == "horizontal":
+                    x = (x / (y / min_height)).__int__()
+                    y = min_height
+                elif composition_type == "vertical":
+                    y = (y * min_width / x).__int__()
+                    x = min_width
 
-            resized_image = new_image.resize((x, y))
-            self.cropped_images.append(resized_image)
+                resized_image = new_image.resize((x, y))
+                self.cropped_images.append(resized_image)
+
+        else:
+            self.__resize_images(composition_type="vertical")
+
+            temp_image = []
+            counter = 0
+            for i, img in enumerate(self.cropped_images):
+                temp_image.append(img)
+
+                if len(temp_image) == 2:
+                    width = min([image.size[0] for image in temp_image])
+                    height = max([image.size[1] for image in temp_image])
+
+                    for image in temp_image:
+                        resized_image = Image.new("RGB", size=(width, height))
+                        resized_image.paste(image, (0, int((height - image.size[1])/2)))
+                        self.cropped_images[counter] = resized_image
+                        counter +=1
+
+                    temp_image.clear()
+
 
     def __add_text(self, image, text, position):
         x, y = image.size
@@ -74,11 +99,12 @@ class Meme:
         }
         image = helpers.draw_text_on_image(image, text, text_pos[position][0], text_pos[position][1])
         self.images_w_text.append(image)
+        image.show()
 
 
 if __name__ == '__main__':
-    i1 = Meme(('data/1.jpg', 'data/2.jpg', 'data/2.jpg'), ("Hello", "Not hello", "OLOLO"))
+    i1 = Meme(('data/1.jpg', 'data/2.jpg', 'data/2.jpg' , 'data/1.jpg'), ("1", "2", "3", "4"))
     # i1 = Meme(('data/1.jpg', ), ("Meme", ))
-    im_name = i1.compose_images("vertical", position="up")
+    im_name = i1.compose_images("grid", position="up")
     im = Image.open(f"./results/{im_name}")
     im.show()
--- telegramside.py
+++ telegramside.py
@@ -7,10 +7,6 @@ TOKEN = "5092926905:AAHxp3Pas4cgxmiBCTy_I1eWTiQf6N2h07Y"
 bot = telebot.TeleBot(TOKEN)
 
 
-class Bot():
-    pass
-
-
 @bot.message_handler(content_types=['text', 'photo', 'video'])
 def get_text_messages(message):
     chat_id = message.chat.id
